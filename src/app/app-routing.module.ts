781dc6fb920393f916e45721dab297420cd589c3
Francisco do Vale <juniordev2@gmail.com>
2019-11-29 21:58:44 +0100

    chore(routing): redirect route change

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { CoursePageComponent } from './course/course-page/course-page.component'
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'courses', pathMatch: 'full' },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'courses', component: CoursePageComponent }
 ];
