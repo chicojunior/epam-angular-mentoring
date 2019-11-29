915eb30b711a1cc67eee6d851fcb722294c97d75
Francisco do Vale <juniordev2@gmail.com>
2019-11-29 07:10:13 +0100

    pull merge

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { AuthService } from './common/services';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  public isLogged: boolean;
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.authService.isLogged.subscribe(res => this.isLogged = res);
+  }
+
 }
